6fcd03d80eaf1759672db03212b17a47fb6046d7
souravdutta8012 <souravdutta2108@gmail.com>
2022-10-16 00:28:52 +0530

    changes

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 # myfirstreactnative
 My first react native
 
-## publish
+## build with expo
 expo login
-expo publish
 expo build:android
+
+## build with eas
+eas login
+eas init
+eas update:configure
+eas build:configure
+eas build -p android (start build)
+eas update (to push code to expo repo. ref: https://docs.expo.dev/eas-update/how-eas-update-works/)
--- app.config.ts
+++ app.config.ts
@@ -1,14 +1,34 @@
-import { ExpoConfig } from '@expo/config-types';
+import { ExpoConfig, ConfigContext } from '@expo/config';
 
 // In SDK 47 and higher, use the following import instead:
 // import { ExpoConfig } from 'expo/config';
 
-const config: ExpoConfig = {
+export default ({ config }: ConfigContext): ExpoConfig => ({
+    ...config,
     name: 'myfirstreactnative',
     slug: 'myfirstreactnative',
     android: {
         package: "com.souravdutta2108.myfirstreactnative"
+    },
+    extra: {
+        eas: {
+            projectId: "4e7eebb4-fa6d-4917-a8cd-2570acd27d27"
+        }
+    },
+    runtimeVersion: {
+        policy: "sdkVersion"
+    },
+    updates: {
+        url: "https://u.expo.dev/4e7eebb4-fa6d-4917-a8cd-2570acd27d27"
     }
-};
+});
 
-export default config;
+// const config: ExpoConfig = {
+//     name: 'myfirstreactnative',
+//     slug: 'myfirstreactnative',
+//     android: {
+//         package: "com.souravdutta2108.myfirstreactnative"
+//     }
+// };
+
+// export default config;
